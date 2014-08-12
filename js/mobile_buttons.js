0b7d9f4c525bfa69e0ed1cc1cfcf6b85db9a33dd
OldManInternetz <michaeljs1993@hotmail.com>
2014-08-12 15:30:00 +0800

    add detail page

--- js/mobile_buttons.js
+++ js/mobile_buttons.js
@@ -1,12 +1,12 @@
 // Button that scrolls to top of page
-$('#bottom-nav-1').click(function () {
-	$('html, body').animate({scrollTop : 0},500);
+$('#bottom-nav-1').bind('tap', function () {
+	$('#scrollbox').animate({scrollTop : 0},500);
 	return false;
 });
 
 
 // Button that scrolls to bottom of page
-$('#bottom-nav-2').click(function () {
-	$('html, body').animate({scrollTop : $(document).height()},500);
+$('#bottom-nav-2').bind('tap', function () {
+	$('#scrollbox').animate({scrollTop : $(scrollbox)[0].scrollHeight - $(document).height() + 30},500);
 	return false;
 });
