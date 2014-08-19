8ac23f3f0ea8789f2e7ae878093425f115c6cea6
Michael <michaeljs1993@hotmail.com>
2014-08-19 14:35:18 +0800

    initialised rails app under 'plantsuwa'

---
+++ old_html/README.md
@@ -0,0 +1,4 @@
+CITS3200-Project
+================
+
+A place to work on our CITS3200 group project.
---
+++ plantsuwa/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Plantsuwa::Application.config.secret_key_base = '4c81bd0a7049a9ca61085cc2d8bbc040285c8cf0ddc7f3849438d1ea77628621fa89d6ea1d754eef483e50462f866c899121027033a0fd54ad15c6a9aeae884f'
