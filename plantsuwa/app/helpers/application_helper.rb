b3a9d8d746c89314e1319f8f9c424a3d7edc6fb7
Michael <michaeljs1993@hotmail.com>
2014-08-21 17:33:03 +0800

    added page titles

--- plantsuwa/app/helpers/application_helper.rb
+++ plantsuwa/app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+
+  def base_title
+    "Plants UWA"
+  end
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} - #{page_title}"
+    end
+  end
+
+  def small_title(page_title)
+    if page_title.empty?
+      base_title
+    else
+      page_title
+    end
+  end
+
 end
