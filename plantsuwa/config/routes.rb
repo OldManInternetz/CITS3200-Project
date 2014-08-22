9b19a11987c7963468c08fafe492f8daff347137
Michael <michaeljs1993@hotmail.com>
2014-08-22 15:37:02 +0800

    added buttons to bottom of show page
    Now there are buttons to edit, and create a new plant, from the show page. I also changed the URLs from /current_plants/etc to just /plants/etc because nobody wants to see the word 'current' in front of plants. They're not supposed to know our database secrets. Haha.

--- plantsuwa/config/routes.rb
+++ plantsuwa/config/routes.rb
@@ -4,7 +4,8 @@ Plantsuwa::Application.routes.draw do
   get "welcome/index"
   root 'welcome#index'
 
-  resources :current_plants, :climates, :families, :flower_colours
+  resources :current_plants, :path => 'plants'
+  resources :climates, :families, :flower_colours
   resources :genera, :leaf_colours, :origins, :sizes, :soil_types, :types
 
  
