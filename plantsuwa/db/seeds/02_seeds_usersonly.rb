385f3e172c6bae1197e9ebd4c2452c3898b7c8cb
Michael <michaeljs1993@hotmail.com>
2014-09-11 13:50:19 +0800

    Seeding works correctly now, I think

---
+++ plantsuwa/db/seeds/02_seeds_usersonly.rb
@@ -0,0 +1,33 @@
+puts "Seeding main users..."
+
+# Admins
+User.create(username: 'Mr Test', password: 'password', password_confirmation: 'password', email: 'mrtest@test.com', admin: 'true', trusted: 'true')
+User.create(username: 'Michael', password: 'password', password_confirmation: 'password', email: 'michael@michael.com', admin: 'true', trusted: 'true')
+User.create(username: 'Duncan', password: 'duncan', password_confirmation: 'duncan', email: 'duncan@duncan.com', admin: 'true', trusted: 'true')
+
+# Trusted Users
+User.create(username: 'Trustworthy', password: 'password', password_confirmation: 'password', email: 'trustme@really.com', admin: 'false', trusted: 'true')
+User.create(username: 'Worthy User', password: 'password', password_confirmation: 'password', email: 'worthy@great.com', admin: 'false', trusted: 'true')
+User.create(username: 'Legitimate', password: 'password', password_confirmation: 'password', email: 'imlegit@hotmail.com', admin: 'false', trusted: 'true')
+
+# Regular Users
+
+User.create(username: 'Don', password: 'password', password_confirmation: 'password', email: 'don@madmen.com', admin: 'false', trusted: 'false')
+User.create(username: 'Sally', password: 'password', password_confirmation: 'password', email: 'sally@madmen.com', admin: 'false', trusted: 'false')
+User.create(username: 'Bobby', password: 'password', password_confirmation: 'password', email: 'bobby@madmen.com', admin: 'false', trusted: 'false')
+User.create(username: 'Betty', password: 'password', password_confirmation: 'password', email: 'betty@madmen.com', admin: 'false', trusted: 'false')
+User.create(username: 'Roger', password: 'password', password_confirmation: 'password', email: 'roger@madmen.com', admin: 'false', trusted: 'false')
+User.create(username: 'Harry', password: 'password', password_confirmation: 'password', email: 'harry@madmen.com', admin: 'false', trusted: 'false')
+User.create(username: 'Joan', password: 'password', password_confirmation: 'password', email: 'joan@madmen.com', admin: 'false', trusted: 'false')
+User.create(username: 'Peggy', password: 'password', password_confirmation: 'password', email: 'peggy@madmen.com', admin: 'false', trusted: 'false')
+User.create(username: 'Mitch', password: 'password', password_confirmation: 'password', email: 'mitch@madmen.com', admin: 'false', trusted: 'false')
+User.create(username: 'Peter', password: 'password', password_confirmation: 'password', email: 'peter@madmen.com', admin: 'false', trusted: 'false')
+User.create(username: 'Duck', password: 'password', password_confirmation: 'password', email: 'duck@madmen.com', admin: 'false', trusted: 'false')
+User.create(username: 'Salvatore', password: 'password', password_confirmation: 'password', email: 'salvatore@madmen.com', admin: 'false', trusted: 'false')
+User.create(username: 'Trudy', password: 'password', password_confirmation: 'password', email: 'trudy@madmen.com', admin: 'false', trusted: 'false')
+
+
+
+
+
+
