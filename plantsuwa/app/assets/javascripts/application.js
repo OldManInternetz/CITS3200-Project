205cdbf799f19dc28b981589d550c098388266f3
duncalish <Duncan@Duncans-MacBook-2.local>
2014-10-12 18:03:03 +0800

    added lat and long to plant forms
    might not bother to figure out how to click on the map for lats and
    longs. we can do it later if necessary.

--- plantsuwa/app/assets/javascripts/application.js
+++ plantsuwa/app/assets/javascripts/application.js
@@ -13,12 +13,20 @@
 //= require jquery
 //= require jquery_ujs
 
+function show_more() {
+	document.getElementById('show_more').classList.remove('visible');
+	document.getElementById('show_more').classList.add('invisible');
+	document.getElementById('show_less').classList.remove('invisible');
+	document.getElementById('show_less').classList.add('visible');
+	document.getElementById('more_locations').classList.remove('invisible');
+	document.getElementById('more_locations').classList.add('visible');
+}
 
-
-/*
-$(function() {
-  $(window.applicationCache).bind("error", function() {
-    alert("There was an error when loading the cache manifest. Please restart the server.");
-  });
-});
-*/
+function show_less() {
+	document.getElementById('more_locations').classList.remove('visible');
+	document.getElementById('more_locations').classList.add('invisible');
+	document.getElementById('show_more').classList.remove('invisible');
+	document.getElementById('show_more').classList.add('visible');
+	document.getElementById('show_less').classList.remove('visible');
+	document.getElementById('show_less').classList.add('invisible');
+}
